ce4335c5b33c9f202f461ab91d47c968d1d40cf0
Matthew Clifford <mhmcliff@gmail.com>
2023-01-23 13:49:50 -0800

    Update Welcome Message

--- TheDeclarationOfIndependence.py
+++ TheDeclarationOfIndependence.py
@@ -1,3 +1,13 @@
 # Programmer: Matthew Clifford
 # Date: 1.20.23
-# Program: Infotech center upgrades
+# Program: InfoTech center upgrades
+
+"""
+Our Welcome Screen will start our program letting
+drivers know that the InfoTech Center 4.0 OS is loading
+"""
+
+print("\n\nWelcome - InfoTech Center 4.0")
+print("\nInfoTech Center 4.0 OS is Loading...")
+
+
