3565f2d2cc69bd55d9cc4d1a732f3b137b68fc8d
Matthew Clifford <mhmcliff@gmail.com>
2023-01-24 14:07:18 -0800

    add ... to loading

--- TheDeclarationOfIndependence.py
+++ TheDeclarationOfIndependence.py
@@ -7,10 +7,27 @@ Our Welcome Screen will start our program letting
 drivers know that the InfoTech Center 4.0 OS is loading
 """
 # Import Libraries Here
+import time, sys, colorama
 from time import sleep
+
 
 print("\n\nWelcome - InfoTech Center 4.0")
 sleep(2)
-print("\nInfoTech Center 4.0 OS is Loading...")
 
+x = 0
+a = 0
 
+print('')
+
+while x != 20:
+    x += 1
+    b = ("InfoTech Center 4.0 OS is Loading" + "." * a)
+    a = a + 1
+# so `b` is printed on top of the previous line.
+    sys.stdout.write('\r'+b)
+
+    time.sleep(0.5)
+    if a == 4:
+        a = 0
+    if x == 20:
+        print('Done!')
