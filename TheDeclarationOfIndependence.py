1816f14c12352bca1cd854a5f2c716272cb45828
Matthew Clifford <mhmcliff@gmail.com>
2023-01-31 13:23:37 -0800

    Updated the date

--- TheDeclarationOfIndependence.py
+++ TheDeclarationOfIndependence.py
@@ -1,3 +1,31 @@
 # Programmer: Matthew Clifford
-# Date: 1.20.23
-# Program: Infotech center upgrades
+# Date: 1.30.23
+# Program: Infotech center 4.0 - Gasoline
+
+"""
+We will create a function that will tell us our Fuel Gage Level
+   - Create a List with Gas Levels
+   - Randomize a choice in the list to determine our gas level
+
+Create a function to determine our closet Gas Station
+   - Create a list of gas stations
+   - Randomly choose a gas station from the list
+
+Create a function to determine our gas level and closest gas station
+   - print Gas Level
+   - print Closest gas station
+"""
+# Import Libraries Here
+import random
+
+
+# Gas Level Function
+def gas_level_gauge():
+    gas_level_list = ["Empty", "Low", "Quarter Tank", "Half Tank", "Three Quarter Tank", "Full Tank"]
+    current_gas_level = random.choice(gas_level_list)
+    return current_gas_level
+
+
+print(gas_level_gauge())
+
+
